1cea78644f0d237e55af142658b507f9ab6b901c
Pardeep <pboora@gmail.com>
2022-01-23 12:37:32 +0530

    Initial commit

---
+++ pboora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pboora
+//
+//  Created by Bakul Boora on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pbooraUITests/pbooraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pbooraUITestsLaunchTests.swift
+//  pbooraUITests
+//
+//  Created by Bakul Boora on 23/01/22.
+//
+
+import XCTest
+
+class pbooraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
